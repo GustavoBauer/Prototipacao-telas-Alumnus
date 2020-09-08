38830567f88bb951f5f731805322884b2b924956
Gustavo Bauer <gbauerbr2001@gmail.com>
2020-09-08 20:37:33 -0300

    Tela de login

---
+++ js/monstroLogin.js
@@ -0,0 +1,88 @@
+var monster = document.getElementById("monster");
+var loginEmail = document.getElementById("loginEmail");
+var signupEmail = document.getElementById("signupEmail");
+var nickname = document.getElementById("signupNickname");
+
+var follow = true;
+
+var w = window.innerWidth / 2;
+
+var h = (window.innerHeight * 20) / 100;
+
+document.onmousemove = function (mouse) {
+  if (follow) {
+    if (mouse.clientX < w && mouse.clientY < h) {
+      monster.src = "../img/monstro-login/idle/2.png";
+    } else if (mouse.clientX < w && mouse.clientY > h) {
+      monster.src = "../img/monstro-login/idle/3.png";
+    } else if (mouse.clientX > w && mouse.clientY < h) {
+      monster.src = "../img/monstro-login/idle/5.png";
+    } else if (mouse.clientX > w && mouse.clientY > h) {
+      monster.src = "../img/monstro-login/idle/4.png";
+    }
+  }
+};
+
+//Melhorar este codigo do oninput
+
+loginEmail.oninput = function () {
+  num = loginEmail.value.length;
+  switch (true) {
+    case num >= 1 && num < 8:
+      monster.src = "../img/monstro-login/read/1.png";
+      break;
+    case num >= 8 && num < 24:
+      monster.src = "../img/monstro-login/read/2.png";
+      break;
+    case num >= 24:
+      monster.src = "../img/monstro-login/read/3.png";
+      break;
+  }
+};
+
+signupEmail.oninput = function () {
+  num = signupEmail.value.length;
+  switch (true) {
+    case num >= 1 && num < 8:
+      monster.src = "../img/monstro-login/read/1.png";
+      break;
+    case num >= 8 && num < 24:
+      monster.src = "../img/monstro-login/read/2.png";
+      break;
+    case num >= 24:
+      monster.src = "../img/monstro-login/read/3.png";
+      break;
+  }
+};
+
+nickname.oninput = function () {
+  num = nickname.value.length;
+  switch (true) {
+    case num >= 1 && num < 8:
+      monster.src = "../img/monstro-login/read/1.png";
+      break;
+    case num >= 8 && num < 24:
+      monster.src = "../img/monstro-login/read/2.png";
+      break;
+    case num >= 24:
+      monster.src = "../img/monstro-login/read/3.png";
+      break;
+  }
+};
+
+function cover() {
+  follow = false;
+  var cont = 1;
+  const convering = setInterval(function () {
+    monster.src = "../img/monstro-login/cover/" + cont + ".png";
+    cont < 8 ? cont++ : clearInterval(convering);
+  }, 60);
+}
+
+function uncover() {
+  var cont = 7;
+  const unconvering = setInterval(function () {
+    monster.src = "../img/monstro-login/cover/" + cont + ".png";
+    cont > 1 ? cont-- : clearInterval(unconvering), (follow = true);
+  }, 60);
+}
